447cf0b185483b74fbc8e948a6372d6bc38697d4
kasperkhansen <52578478+kasperkhansen@users.noreply.github.com>
2024-05-06 10:58:54 +0200

    dev branch created

--- src/main/java/dk/kea/projekt3_gruppe6_bilabonnement/Projekt3Gruppe6BilAbonnementApplication.java
+++ src/main/java/dk/kea/projekt3_gruppe6_bilabonnement/Projekt3Gruppe6BilAbonnementApplication.java
@@ -2,7 +2,7 @@ package dk.kea.projekt3_gruppe6_bilabonnement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class Projekt3Gruppe6BilAbonnementApplication {
 
