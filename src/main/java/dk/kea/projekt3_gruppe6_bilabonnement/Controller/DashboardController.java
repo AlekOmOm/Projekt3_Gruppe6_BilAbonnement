2b2ed72eedc5f7ced36b964b300013da04ae6c5f
Magnus Munk Braunstein <147395199+MagnusMunkBraunstein@users.noreply.github.com>
2024-05-22 14:31:14 +0200

    ModelAtribute og Maping

--- src/main/java/dk/kea/projekt3_gruppe6_bilabonnement/Controller/DashboardController.java
+++ src/main/java/dk/kea/projekt3_gruppe6_bilabonnement/Controller/DashboardController.java
@@ -3,6 +3,7 @@ import dk.kea.projekt3_gruppe6_bilabonnement.Service.DashboardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class DashboardController {
@@ -11,17 +12,21 @@ public class DashboardController {
     public DashboardController(DashboardService dashboardService) {
         this.dashboardService = dashboardService;
     }
-    @GetMapping("/dashboard")
-    public String seDashboard(Model model) {
+
+    @ModelAttribute
+    public void addAttributes(Model model) {
         model.addAttribute("AntalUdlejedeBiler", dashboardService.seAntalUdlejdeBiler());
         model.addAttribute("SamletIndkomstForBiler", dashboardService.seTotalIndkomst());
+    }
+
+    @GetMapping("/dashboard")
+    public String seDashboard(Model model) {
         return "dashboard";
     }
 
+    @GetMapping("/forretningsrapport")
+    public String seForretningsRapport(Model model) {
+    return "forretningsrapport";
+    }
 
-
-    @GetMapping("/ForretningsRapport")
-        public String seForretningsRapport(Model model) {
-            return "ForretningsRapport";
-        }
 }
