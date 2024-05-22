a3788d85c438a13f2251fee928a08bc27070883e
kasperkhansen <52578478+kasperkhansen@users.noreply.github.com>
2024-05-22 10:45:46 +0200

    Start på Userstory 5 Se og Opbevar Skade Rapport

--- src/main/java/dk/kea/projekt3_gruppe6_bilabonnement/Service/LejeAftaleService.java
+++ src/main/java/dk/kea/projekt3_gruppe6_bilabonnement/Service/LejeAftaleService.java
@@ -1,7 +1,21 @@
 package dk.kea.projekt3_gruppe6_bilabonnement.Service;
 
+import dk.kea.projekt3_gruppe6_bilabonnement.Model.LejeAftale;
+import dk.kea.projekt3_gruppe6_bilabonnement.Repository.LejeAftaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LejeAftaleService {
+
+    @Autowired
+    LejeAftaleRepository lejeAftaleRepository;
+
+
+    public List<LejeAftale> getLejeAftaleUdenRapport(){
+        return lejeAftaleRepository.getLejeAftalerUdenRapport();
+    }
 }
